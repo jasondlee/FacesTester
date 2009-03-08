25e6c101f2c81a8d91102d3ee1222e537fe315f3
rcoffin@localhost <rcoffin@localhost>
2009-03-08 18:12:45 -0500

    Beginning simple form test

--- facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesForm.java
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesForm.java
@@ -1,8 +1,20 @@
 package com.steeplesoft.jsf.facestester;
 
+import javax.faces.component.UIInput;
 import javax.faces.component.html.HtmlForm;
 
 public class FacesForm {
+    private HtmlForm form;
+
     public FacesForm(HtmlForm htmlForm) {
+        form = htmlForm;
+    }
+
+    public void setValue(String fieldName, String value) {
+        UIInput component = (UIInput) form.findComponent(fieldName);
+        component.setValue(value);
+    }
+
+    public void submit() {
     }
 }
--- integration-test/src/test/java/com/steeplesoft/jsf/facestester/sample/WhenSubmittingZipCodeForm.java
+++ integration-test/src/test/java/com/steeplesoft/jsf/facestester/sample/WhenSubmittingZipCodeForm.java
@@ -3,9 +3,6 @@ package com.steeplesoft.jsf.facestester.sample;
 import com.steeplesoft.jsf.facestester.FacesForm;
 import com.steeplesoft.jsf.facestester.FacesPage;
 import com.steeplesoft.jsf.facestester.FacesTester;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class WhenSubmittingZipCodeForm {
@@ -13,8 +10,9 @@ public class WhenSubmittingZipCodeForm {
     public void shouldDisplayCityForZipCode() {
         FacesTester tester = new FacesTester();
         FacesPage page = tester.requestPage("/zipCodeMapper.xhtml");
-        FacesForm form = page.getFormById("form");
 
-        assertThat(form, is(notNullValue()));
+        FacesForm form = page.getFormById("form");
+        form.setValue("zipCode", "95054");
+        form.submit();
     }
 }
