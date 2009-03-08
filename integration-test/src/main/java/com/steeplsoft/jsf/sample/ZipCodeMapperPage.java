15b3a5be546c97822d8e9576f5f56656cd53a0ed
rcoffin@localhost <rcoffin@localhost>
2009-03-08 16:17:32 -0500

    Simple form submission example

---
+++ integration-test/src/main/java/com/steeplsoft/jsf/sample/ZipCodeMapperPage.java
@@ -0,0 +1,31 @@
+package com.steeplsoft.jsf.sample;
+
+public class ZipCodeMapperPage {
+    private String zipCode;
+    private String city;
+
+    public String lookupCityForZip() {
+        if ("95054".equals(zipCode)) {
+            city = "Santa Clara";
+        } else {
+            city = "Unknown";
+        }
+        return "success";
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
