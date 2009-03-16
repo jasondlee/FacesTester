b16c571eca392e434cd02aa948e4a23e5daa0bc3
Jason Lee <jason>
2009-03-16 15:49:50 -0500

    Add method to dump the component tree to stdout

--- facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesComponent.java
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesComponent.java
@@ -32,4 +32,18 @@ public class FacesComponent extends AbstractFacesComponent {
 
         return new FacesForm(form);
     }
+
+    public void dumpComponentTree() {
+        System.out.println("/ " + this.getWrappedComponent().getId());
+        for (UIComponent child : this.getWrappedComponent().getChildren()) {
+            dumpChildren(child, "----");
+        }
+    }
+
+    private void dumpChildren(UIComponent component, String prefix) {
+        System.out.println("|" + prefix + "----" + component.getId());
+        for (UIComponent child : component.getChildren()) {
+            dumpChildren(child, prefix+"----");
+        }
+    }
 }
