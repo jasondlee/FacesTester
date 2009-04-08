619b62c73a4402ab196bc434d82b976a16624c19
Jason Lee <jason>
2009-04-08 07:24:25 -0500

    Added missing files. :|

---
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/metadata/PhaseListenerMetaData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.steeplesoft.jsf.facestester.metadata;
+
+/**
+ *
+ * @author jasonlee
+ */
+public class PhaseListenerMetaData {
+    protected String className;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
---
+++ facestester/src/test/java/com/steeplesoft/jsf/facestester/TestPhaseListener.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.steeplesoft.jsf.facestester;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+/**
+ *
+ * @author jasonlee
+ */
+public class TestPhaseListener implements PhaseListener {
+
+    public void afterPhase(PhaseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void beforePhase(PhaseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public PhaseId getPhaseId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
