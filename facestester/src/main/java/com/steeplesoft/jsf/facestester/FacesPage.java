e9ce9e169d1cff89a89b23e00f8898173acc0c66
Jason Lee <jason>
2009-04-13 08:31:52 -0500

    These files weren't saved after the reformat :|

--- facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesPage.java
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesPage.java
@@ -1,22 +1,26 @@
 package com.steeplesoft.jsf.facestester;
 
+import org.springframework.mock.web.MockHttpServletResponse;
+
 import java.io.UnsupportedEncodingException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlForm;
 import javax.faces.context.FacesContext;
-import org.springframework.mock.web.MockHttpServletResponse;
+
 
 public class FacesPage extends FacesComponent {
-
-    private FacesContextBuilder facesContextBuilder;
     private FacesContext facesContext;
+    private FacesContextBuilder facesContextBuilder;
     private FacesLifecycle lifecycle;
     private String uri;
 
-    public FacesPage(FacesContext facesContext, FacesContextBuilder facesContextBuilder, FacesLifecycle lifecycle,
-                     String uri) {
+    public FacesPage(FacesContext facesContext,
+        FacesContextBuilder facesContextBuilder, FacesLifecycle lifecycle,
+        String uri) {
         super(facesContext.getViewRoot());
 
         this.facesContextBuilder = facesContextBuilder;
@@ -36,28 +40,36 @@ public class FacesPage extends FacesComponent {
     public FacesForm getFormById(String id) {
         for (UIComponent each : component.getChildren()) {
             if (each instanceof HtmlForm && id.equals(each.getId())) {
-                return new FacesForm((HtmlForm) each, facesContextBuilder, lifecycle, uri);
+                return new FacesForm((HtmlForm) each, facesContextBuilder,
+                    lifecycle, uri);
             }
         }
-        throw new AssertionError("HtmlForm '" + id + "' does not exist on page.");
+
+        throw new AssertionError("HtmlForm '" + id +
+            "' does not exist on page.");
+    }
+
+    public String getParameterValue(String key) {
+        return facesContext.getExternalContext().getRequestParameterMap()
+                           .get(key);
     }
 
     public String getRenderedPage() {
         String renderedPage = "";
+
         try {
-            renderedPage = ((MockHttpServletResponse) facesContext.getExternalContext().getResponse()).getContentAsString();
+            renderedPage = ((MockHttpServletResponse) facesContext.getExternalContext()
+                                                                  .getResponse()).getContentAsString();
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(FacesPage.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(FacesPage.class.getName())
+                  .log(Level.SEVERE, null, ex);
         }
+
         return renderedPage;
     }
 
     @Override
     public String getValueAsString() {
         return null;
-    }
-
-    public String getParameterValue(String key) {
-        return facesContext.getExternalContext().getRequestParameterMap().get(key);
     }
 }
--- facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesTester.java
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/FacesTester.java
@@ -2,45 +2,53 @@ package com.steeplesoft.jsf.facestester;
 
 import com.steeplesoft.jsf.facestester.metadata.FacesConfig;
 import static com.steeplesoft.jsf.facestester.servlet.ServletContextFactory.createServletContext;
+
 import org.springframework.mock.web.MockHttpServletResponse;
+
 import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import static java.lang.String.format;
 
 import javax.faces.FactoryFinder;
 import static javax.faces.FactoryFinder.LIFECYCLE_FACTORY;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.lifecycle.LifecycleFactory;
 import static javax.faces.lifecycle.LifecycleFactory.DEFAULT_LIFECYCLE;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
+
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import javax.faces.component.UIComponent;
-import static java.lang.String.format;
 
 /**
  * @author jasonlee
  */
 public class FacesTester {
-
-    private FacesLifecycle lifecycle;
     private FacesContextBuilder facesContextBuilder;
+    private FacesLifecycle lifecycle;
 
     public FacesTester() {
         facesContextBuilder = new FacesContextBuilderImpl(createServletContext());
 
         LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(LIFECYCLE_FACTORY);
-        lifecycle = new FacesLifecycleImpl(factory.getLifecycle(DEFAULT_LIFECYCLE));
+        lifecycle = new FacesLifecycleImpl(factory.getLifecycle(
+                    DEFAULT_LIFECYCLE));
     }
 
     public FacesComponent createComponent(String componentType) {
-        FacesContext context = facesContextBuilder.createFacesContext("/dummyPage.xhtml", "GET", lifecycle);
+        FacesContext context = facesContextBuilder.createFacesContext("/dummyPage.xhtml",
+                "GET", lifecycle);
         lifecycle.execute(context);
-        return new FacesComponent(context.getApplication().createComponent(componentType));
+
+        return new FacesComponent(context.getApplication()
+                                         .createComponent(componentType));
     }
 
     public FacesPage requestPage(String uri) {
-        FacesContext context = facesContextBuilder.createFacesContext(uri, "GET", lifecycle);
+        FacesContext context = facesContextBuilder.createFacesContext(uri,
+                "GET", lifecycle);
 
         lifecycle.execute(context);
         lifecycle.render(context);
@@ -50,27 +58,33 @@ public class FacesTester {
         return new FacesPage(context, facesContextBuilder, lifecycle, uri);
     }
 
-    private void checkForErrors(FacesContext context) {
-        MockHttpServletResponse response = (MockHttpServletResponse) context.getExternalContext().getResponse();
-        if (SC_OK == response.getStatus()) {
-            return;
-        }
-        switch (response.getStatus()) {
-            case SC_NOT_FOUND:
-                throw new FacesTesterException(format("The page %s was not found.", response.getErrorMessage()));
-            default:
-                throw new FacesTesterException(response.getErrorMessage());
-        }
-    }
-
-    public void validateFacesConfig(String path) throws IOException, SAXException, ParserConfigurationException {
+    public void validateFacesConfig(String path)
+        throws IOException, SAXException, ParserConfigurationException {
         validateFacesConfig(new File(path));
     }
 
-    public void validateFacesConfig(File file) throws IOException, SAXException, ParserConfigurationException {
+    public void validateFacesConfig(File file)
+        throws IOException, SAXException, ParserConfigurationException {
         FacesConfig config = new FacesConfig(file);
         config.validateManagedBeans();
         config.validateComponents();
+    }
 
+    private void checkForErrors(FacesContext context) {
+        MockHttpServletResponse response = (MockHttpServletResponse) context.getExternalContext()
+                                                                            .getResponse();
+
+        if (SC_OK == response.getStatus()) {
+            return;
+        }
+
+        switch (response.getStatus()) {
+        case SC_NOT_FOUND:
+            throw new FacesTesterException(format("The page %s was not found.",
+                    response.getErrorMessage()));
+
+        default:
+            throw new FacesTesterException(response.getErrorMessage());
+        }
     }
 }
