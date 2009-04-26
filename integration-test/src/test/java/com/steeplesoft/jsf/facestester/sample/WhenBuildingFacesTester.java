1364335c3eae7623b5de59c4f8ba79fbad6bfa8b
rcoffin@rodsmacpro.local <rcoffin@rodsmacpro.local>
2009-04-26 18:55:05 -0500

    Testing scenarios asked about in kenai forum

--- integration-test/src/test/java/com/steeplesoft/jsf/facestester/sample/WhenBuildingFacesTester.java
+++ integration-test/src/test/java/com/steeplesoft/jsf/facestester/sample/WhenBuildingFacesTester.java
@@ -6,20 +6,51 @@
 package com.steeplesoft.jsf.facestester.sample;
 
 import com.steeplesoft.jsf.facestester.FacesTester;
+import com.steeplesoft.jsf.sample.ZipCodeMapperPage;
+
 import javax.faces.component.html.HtmlOutputText;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import javax.el.ELContext;
+import javax.el.ELResolver;
+
 import org.junit.Test;
+import org.junit.Before;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 /**
- *
  * @author jasonlee
  */
 public class WhenBuildingFacesTester {
+    private FacesTester tester;
+
+    @Before
+    public void setUp() {
+        tester = new FacesTester();
+    }
+
     @Test
     public void shouldBeAbleToCreateStandardComponents() {
-        FacesTester facesTester = new FacesTester();
-        assertThat(facesTester.createComponent(HtmlOutputText.COMPONENT_TYPE).getWrappedComponent() instanceof HtmlOutputText,
+        assertThat(tester.createComponent(HtmlOutputText.COMPONENT_TYPE).getWrappedComponent() instanceof HtmlOutputText,
                 is(true));
+    }
+
+    @Test
+    public void shouldBeAbleToAddFacesMessage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("add entry failed - see log for details"));
+    }
+
+    @Test
+    public void shouldBeAbleToAccessManagedBeans() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ELResolver elResolver = context.getApplication().getELResolver();
+        ELContext elContext = context.getELContext();
+        ZipCodeMapperPage backingBean = (ZipCodeMapperPage) elResolver.getValue(elContext, null, "ZipCodeMapperPage");
+
+        assertThat(backingBean, is(not(nullValue())));
     }
 }
