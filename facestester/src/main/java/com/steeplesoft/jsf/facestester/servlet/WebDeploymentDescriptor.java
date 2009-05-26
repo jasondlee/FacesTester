e49621b0e8a8136c1214bd3c50c3c1d10bdb9f50
Jason Lee <jason>
2009-05-26 17:18:51 -0500

    Add support for listeners to WebDeploymentDescriptor

--- facestester/src/main/java/com/steeplesoft/jsf/facestester/servlet/WebDeploymentDescriptor.java
+++ facestester/src/main/java/com/steeplesoft/jsf/facestester/servlet/WebDeploymentDescriptor.java
@@ -1,16 +1,38 @@
 package com.steeplesoft.jsf.facestester.servlet;
 
+import com.steeplesoft.jsf.facestester.Util;
+import java.io.File;
 import java.io.InputStream;
 
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class WebDeploymentDescriptor {
-    private Map<String, String> contextParameters;
+    protected File webAppPath;
+    private Map<String, String> contextParameters = new HashMap<String, String>();
+    private List<EventListener> listeners = new ArrayList<EventListener>();
 
-    public static WebDeploymentDescriptor createFromStream(
-        InputStream webXmlStream) {
-        return new WebDeploymentDescriptorParser().parse(webXmlStream);
+    public WebDeploymentDescriptor() {
+    }
+
+    public WebDeploymentDescriptor(File webAppPath) {
+        this.webAppPath = webAppPath;
+    }
+
+    public static WebDeploymentDescriptor createFromFile(File webAppPath) {
+        return new WebDeploymentDescriptorParser().parse(webAppPath);
+    }
+
+    public File getWebAppPath() {
+        return webAppPath;
+    }
+
+    public void setWebAppPath(File webAppPath) {
+        this.webAppPath = webAppPath;
     }
 
     public Map<String, String> getContextParameters() {
@@ -19,5 +41,13 @@ public class WebDeploymentDescriptor {
 
     public void setContextParameters(Map<String, String> contextParameters) {
         this.contextParameters = contextParameters;
+    }
+
+    public List<EventListener> getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(List<EventListener> listeners) {
+        this.listeners = listeners;
     }
 }
