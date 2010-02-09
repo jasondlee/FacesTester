edcff1ec054c0d22722e504b8d1953dc619bd560
Jason Lee <jason@steeplesoft.com>
2010-02-09 14:45:50 -0600

    Weld integration changes...

--- facestester-connector-weld/src/main/java/com/steeplesoft/jsf/facestester/weld/WeldApplication.java
+++ facestester-connector-weld/src/main/java/com/steeplesoft/jsf/facestester/weld/WeldApplication.java
@@ -1,5 +1,8 @@
 package com.steeplesoft.jsf.facestester.weld;
 
+import org.jboss.weld.bootstrap.WeldBootstrap;
+import org.jboss.weld.bootstrap.spi.BeanDeploymentArchive;
+
 import javax.el.ExpressionFactory;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.faces.application.Application;
@@ -8,6 +11,7 @@ import javax.faces.application.ApplicationWrapper;
 public class WeldApplication extends ApplicationWrapper {
     private Application delegate;
     private ExpressionFactory expressionFactory;
+    private BeanManager beanManager;
 
     public WeldApplication(Application application) {
         delegate = application;
@@ -31,9 +35,14 @@ public class WeldApplication extends ApplicationWrapper {
     }
 
     private BeanManager getBeanManager() {
-//            InitialContext context = new InitialContext();
-//            return (BeanManager) context.lookup("java:comp/BeanManager");
-        return null;
+        if (beanManager == null) {
+            BeanDeploymentArchive bda = weldDeployer.getBeanDeploymentArchiveForBundle(bundle);
+            if( bda != null ) {
+                WeldBootstrap bootstrap = weldDeployer.getBootstrapForApp(bundle.getApplication());
+                beanManager = bootstrap.getManager(bda);
+            }
+        }
+        return beanManager;
 
     }
 }
